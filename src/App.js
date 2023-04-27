9b69d81777bfb0923fe8c8f005423da00eec5a46
tanas <tanasoaiaanamaria@gmail.com>
2023-04-28 00:50:54 +0300

    Adaugare imagine login plus rute private

--- src/App.js
+++ src/App.js
@@ -6,12 +6,15 @@ import {
   useLocation,
 } from "react-router-dom";
 
+import ProtectedRoute from "./rute/ProtectedRoutes";
+
 import Navigare from "./componente/Navigare/Navigare";
-import Subsol from './componente/Subsol/Subsol.jsx'
+import Subsol from "./componente/Subsol/Subsol.jsx";
 
 import PaginaPrincipala from "./pagini/PaginaPrincipala/PaginaPrincipala.jsx";
+import Onboarding from "./pagini/Onboarding/Onboarding";
 
-import useWindowDimensions from './hooks/useWindowDimensions';
+import useWindowDimensions from "./hooks/useWindowDimensions";
 
 function App() {
   const { width } = useWindowDimensions();
@@ -19,17 +22,35 @@ function App() {
     <Router>
       <Routes>
         <Route
-        element={
-          <>
-            <Navigare expand={width >= 750 ? 'md' : false} />
-            <Outlet />
-            <Subsol />
-          </>
-        }>
-          {/* public rute */}
-          <Route path='/' element={<PaginaPrincipala />} />
+          element={
+            <>
+              <Navigare expand={width >= 750 ? "md" : false} />
+              <ProtectedRoute />
+              <Subsol />
+            </>
+          }
+        >
+          {/* protected routes */}
         </Route>
 
+        <Route
+          element={
+            <>
+              <Navigare expand={width >= 750 ? "md" : false} />
+              <Outlet />
+              <Subsol />
+            </>
+          }
+        >
+          {/* public rute */}
+          <Route path="/" element={<PaginaPrincipala />} />
+
+          {/* onboarding routes */}
+          <Route path="/login" element={<Onboarding />} />
+          <Route path="/register" element={<Onboarding />} />
+          <Route path="/forgot-password" element={<Onboarding />} />
+          <Route path="/reset-password" element={<Onboarding />} />
+        </Route>
       </Routes>
     </Router>
   );
---
+++ src/pagini/Onboarding/Onboarding.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useLocation } from "react-router-dom";
+
+import { Container, Row, Col, Form, Button } from "react-bootstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+import LoginImage from "../../assets/imagini/loginForm.jpg";
+import style from "./Onboarding.module.scss";
+import LoginForm from "./Login/LoginForm";
+import RegisterForm from "./Register/RegisterForm";
+import ForgotPasswordForm from "./ForgotPassword/ForgotPasswordForm";
+import ResetPasswordForm from "./ResetPassword/ResetPasswordForm";
+
+const Onboarding = () => {
+  const location = useLocation().pathname;
+  return (
+    <div className={style.mainContainer}>
+      {/* <div className={style.container}> */}
+      <div className={style.leftSide}>
+        <div className={style.contentContainer}>
+          {/* <img
+            src={AssistLogo}
+            className={style.logoImageOnboarding}
+            alt="Login Logo"
+          /> */}
+          <br />
+          <br />
+
+          {location === "/login" && <LoginForm />}
+          {location === "/register" && <RegisterForm />}
+          {location === "/forgot-password" && <ForgotPasswordForm />}
+          {location === "/reset-password" && <ResetPasswordForm />}
+        </div>
+      </div>
+      {/* End leftSide */}
+      <img
+        src={LoginImage}
+        className={style.rightImageOnboarding}
+        alt="Login"
+      />
+    </div>
+  );
+};
+
+export default Onboarding;
